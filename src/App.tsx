d50b3266a81857c80b071e79756850e04be85790
AvDevx <avats765@gmail.com>
2020-10-05 00:46:01 +0530

    Project Initilazied

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// Importing react.
+import React from 'react';
+
+// Importing theme style.
+import './theme/app.css';
+
+
+
+const App: React.FC = () => (
+  <div className="app">
+    <h2>Anshuman vats</h2>
+  </div>
+);
+
+export default App;
